e85024c211fbc1f09572c7d80b29756acbc46a09
M.Said <m.luckysniper@gmail.com>
2024-08-06 01:49:01 +0300

    multi-lingual

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +12,15 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/greeting/{locale}', function (string $locale) {
+    if (! in_array($locale, ['en', 'ar'])) {
+        abort(400);
+    }
+ 
+    App::setLocale($locale);
+
+    return __('messages.welcome');
+ 
+    // ...
+});
